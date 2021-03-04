e51bbb7e26a9175d458f84500a8c4b1f589928f8
Ersin Elci <ersinelci@gmail.com>
2021-03-04 18:35:30 +0300

    Create ADC.c

---
+++ ADC.c
@@ -0,0 +1,41 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+uint16_t adcVal;
+void ADC0_init(void);
+uint16_t ADC0_read(void);
+void ADC0_init(void)
+{
+/* Disable digital input buffer */
+PORTA.PIN6CTRL &= ~PORT_ISC_gm;
+PORTA.PIN6CTRL |= PORT_ISC_INPUT_DISABLE_gc;
+/* Disable pull-up resistor */
+PORTA.PIN6CTRL &= ~PORT_PULLUPEN_bm;
+ADC0.CTRLC = ADC_PRESC_DIV4_gc /* CLK_PER divided by 4 */
+| ADC_REFSEL_INTREF_gc; /* Internal reference */
+ADC0.CTRLA = ADC_ENABLE_bm /* ADC Enable: enabled */
+| ADC_RESSEL_10BIT_gc; /* 10-bit mode */
+/* Select ADC channel */
+ADC0.MUXPOS = ADC_MUXPOS_AIN6_gc;
+}
+uint16_t ADC0_read(void)
+{
+/* Start ADC conversion */
+ADC0.COMMAND = ADC_STCONV_bm;
+/* Wait until ADC conversion done */
+while ( !(ADC0.INTFLAGS & ADC_RESRDY_bm) )
+{
+}
+/* Clear the interrupt flag by writing 1: */
+ADC0.INTFLAGS = ADC_RESRDY_bm;
+return ADC0.RES;
+}
+int main(void)
+{
+ADC0_init();
+adcVal = ADC0_read();
+while (1)
+{
+}
+}
+  
+  
